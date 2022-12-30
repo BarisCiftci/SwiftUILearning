feaa6277af8447cc40e503fc437bcfeb63082025
Baris Ciftci <barishciftci@gmail.com>
2022-12-30 23:53:31 +0100

    Initial Commit

---
+++ SwiftUILearning/SwiftUILearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearningApp.swift
+//  SwiftUILearning
+//
+//  Created by Baris Ciftci on 30/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
