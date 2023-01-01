6c334e34477c9d00c49ae4dc682a577daad70782
Baris Ciftci <barishciftci@gmail.com>
2023-01-01 17:09:53 +0100

    Learned system icons

--- SwiftUILearning/SwiftUILearningGradients.swift
+++ SwiftUILearning/SwiftUILearningGradients.swift
@@ -9,21 +9,26 @@ import SwiftUI
 
 struct SwiftUILearningGradients: View {
     var body: some View {
-        RoundedRectangle(cornerRadius: 25)
-            .fill(
-            //    Color.pink
-//                LinearGradient(
-//                    gradient: Gradient(colors: [Color.blue, Color.pink, Color.orange]),
-//                    startPoint: .bottomLeading,
-//                    endPoint: .topTrailing)
-                
-                RadialGradient(gradient: Gradient(colors: [Color.orange,Color.pink, Color.blue]),
-                               center: .topLeading,
-                               startRadius: 0,
-                               endRadius: 400)
-            )
+        ZStack {
+            RoundedRectangle(cornerRadius: 25)
+                .fill(
+                //    Color.pink
+    //                LinearGradient(
+    //                    gradient: Gradient(colors: [Color.blue, Color.pink, Color.orange]),
+    //                    startPoint: .bottomLeading,
+    //                    endPoint: .topTrailing)
+                    
+                    RadialGradient(gradient: Gradient(colors: [Color.orange,Color.pink, Color.blue]),
+                                   center: .topLeading,
+                                   startRadius: 0,
+                                   endRadius: 400)
+                )
             .frame(width: 300, height: 200)
             
+                
+        }
+        
+        
     }
 }
 
---
+++ SwiftUILearning/SwiftUILearningSystemIcons.swift
@@ -0,0 +1,29 @@
+//
+//  SwiftUILearningSystemIcons.swift
+//  SwiftUILearning
+//
+//  Created by Baris Ciftci on 01/01/2023.
+//
+
+import SwiftUI
+
+struct SwiftUILearningSystemIcons: View {
+    var body: some View {
+        Image(systemName: "folder.badge.plus")
+            .renderingMode(.original)
+            .resizable()
+            .scaledToFill()
+           // .aspectRatio(contentMode: .fit)
+           // .font(.system(size: 100))
+           // .foregroundColor(.yellow)
+            .frame(width: 100, height: 100)
+        //.clipped()
+            
+    }
+}
+
+struct SwiftUILearningSystemIcons_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUILearningSystemIcons()
+    }
+}
