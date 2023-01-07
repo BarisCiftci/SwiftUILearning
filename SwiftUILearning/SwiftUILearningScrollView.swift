d5107b0b9840671c80c7a1b7e2a12628c01da500
Baris Ciftci <barishciftci@gmail.com>
2023-01-07 23:28:50 +0100

    Learned ScrollViews

---
+++ SwiftUILearning/SwiftUILearningScrollView.swift
@@ -0,0 +1,51 @@
+//
+//  SwiftUILearningScrollView.swift
+//  SwiftUILearning
+//
+//  Created by Baris Ciftci on 07/01/2023.
+//
+
+import SwiftUI
+
+struct SwiftUILearningScrollView: View {
+    var body: some View {
+        
+        ScrollView {
+            ForEach(0..<10) { index in
+                
+                Text("Hello".uppercased())
+                    .font(.system(size: 120))
+                    .fontWeight(.black)
+                    .foregroundColor(.pink)
+                
+            }
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    ForEach(0..<100) { index in
+                        
+                        Text("Hello".uppercased())
+                            .font(.system(size: 120))
+                            .fontWeight(.black)
+                            .foregroundColor(.blue)
+                        
+                    }
+                }
+            }
+            ForEach(0..<10) { index in
+                
+                Text("Hello".uppercased())
+                    .font(.system(size: 120))
+                    .fontWeight(.black)
+                    .foregroundColor(.orange)
+                
+            }
+        }
+        
+    }
+}
+
+struct SwiftUILearningScrollView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUILearningScrollView()
+    }
+}
