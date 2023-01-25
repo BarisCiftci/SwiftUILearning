5e4b84296b001dbe37015b862b37dbf0256c788e
Baris Ciftci <barishciftci@gmail.com>
2023-01-25 20:15:51 +0100

    learned about Picker

---
+++ SwiftUILearning/SwiftUILearningPicker.swift
@@ -0,0 +1,41 @@
+//
+//  SwiftUILearningPicker.swift
+//  SwiftUILearning
+//
+//  Created by Baris Ciftci on 25/01/2023.
+//
+
+import SwiftUI
+
+struct SwiftUILearningPicker: View {
+    
+    @State var selection: String = "1"
+    
+    var body: some View {
+        VStack {
+            HStack{
+                Text("Age:")
+                Text(selection)
+            }
+            Picker(
+                selection: $selection,
+                label: Text("Hello"),
+                content: {
+                    ForEach(18..<124) { number in
+                        Text("\(number)")
+                            .tag("\(number)")
+                    }
+            })
+            .pickerStyle(.wheel)
+            .background(Color.red)
+            .cornerRadius(20)
+            .padding(.horizontal, 20)
+        }
+    }
+}
+
+struct SwiftUILearningPicker_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUILearningPicker()
+    }
+}
