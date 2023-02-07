60095c7da5820e90dcf8ee11e120f2dde30ce66a
Baris Ciftci <barishciftci@gmail.com>
2023-02-07 20:27:13 +0100

    Learned .textSelection

---
+++ SwiftUILearning/SwiftUILearninTextSelection.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUILearninTextSelection.swift
+//  SwiftUILearning
+//
+//  Created by Baris Ciftci on 07/02/2023.
+//
+
+import SwiftUI
+
+struct SwiftUILearninTextSelection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .textSelection(.enabled)
+    }
+}
+
+struct SwiftUILearninTextSelection_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUILearninTextSelection()
+    }
+}
